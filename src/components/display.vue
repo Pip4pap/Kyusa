72e53402b0b9e522c6febeab7bff21e28a9c292f
Musumba Phillip Mulindwa <phillip.musumba.52@gmail.com>
2018-12-14 17:15:16 +0300

    feat: Fetch conversion rate according to input currencies

--- src/components/display.vue
+++ src/components/display.vue
@@ -1,11 +1,57 @@
 <template>
-	<p></p>
+	<p>{{ result }}</p>
 </template>
 
 <script>
-export default{
+export default {
   name: 'display',
   props: ['toCurrency', 'fromCurrency'],
+  data() {
+  	return {
+  		result: null
+  	}
+  },
+  watch: {
+  	toCurrency: function(_) {
+  		runConversion(this.fromCurrency, this.toCurrency).then(result => {
+  			this.result = round(result, 0)
+  		})
+  	},
+  	fromCurrency: function(_) {
+  		runConversion(this.fromCurrency, this.toCurrency).then(result => {
+  			this.result = round(result, 0)
+  		})
+  	}
+  },
+
+}
+
+function runConversion(from, to) {
+	console.log('running conversion', from, to)
+	return new Promise((resolve, reject) => {
+		if (from && to) {
+			getRate(from, to).then(rate => {
+				resolve(rate * 1000)
+			})
+		}
+	})
+}
+
+function round (value, decimals) {
+	return Number(Math.round(`${value}e${decimals}`)+`e-${decimals}`)
+}
+
+function getRate(from, to) {
+	return fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=ultra`)
+			.then(resp => {
+				return resp.json()
+			})
+		  	.then(data => {
+		  		// this.rate = Object.values(data)[0]
+		  		let rate = Object.values(data)[0]
+		  		console.log(rate)
+		  		return rate
+		  	})
 }
 </script>
 
