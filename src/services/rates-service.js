8531d9a35b7fd92a1876d701129147f9429da9a7
Musumba Phillip Mulindwa <phillip.musumba.52@gmail.com>
2018-12-20 17:39:35 +0300

    feat: Fetch currencies from IDB in offline-mode

--- src/services/rates-service.js
+++ src/services/rates-service.js
@@ -1,11 +1,41 @@
+import idb from "@/assets/js/idb"
+
+function openIDB(){
+	if(!'serviceWorker' in navigator){
+	  return Promise.resolve();
+	} else {
+		return idb.open('currencyDB', 1, upgradeDB => {
+	      const currrencies = upgradeDB.createObjectStore('currencies')
+		})
+	}
+}
+
 function getCurrencies() {
-	return fetch('https://free.currencyconverterapi.com/api/v5/currencies')
-  			.then(response => {
-  				return response.json()
-  			})
-  			.then(data => {
-  				return data.results	
-  			})
+	let returnPromise = openIDB().then(db => {
+	  	const currencyStore = db.transaction(['currencies']).objectStore('currencies')
+	  	return currencyStore.get('currencies').then(data => {
+	  		console.log('From the IDB', data)
+	  		if(data === undefined){
+			    return fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+				        .then(response => {
+				          return response.json();
+				        })
+				        .then(data => {
+				        	let currencies = data.results	
+							openIDB().then(db => {
+								let tx = db.transaction('currencies', 'readwrite');
+							    let store = tx.objectStore('currencies');
+							    store.put(currencies, 'currencies')
+							})
+				    		console.log('From the network:',currencies)
+				    		return currencies
+				        })
+	  		}
+	  		return data
+	  	})
+	})
+    console.log('returnPromise', returnPromise)
+    return returnPromise
 }
 
 function getRate(fromCurrency, toCurrency){
