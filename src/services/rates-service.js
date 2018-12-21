44475f273789eb3615d5efff5c710fe4736a30b4
Musumba Phillip Mulindwa <phillip.musumba.52@gmail.com>
2018-12-21 16:22:43 +0300

    feat: Fetch conversion rate from IDB if available

--- src/services/rates-service.js
+++ src/services/rates-service.js
@@ -2,9 +2,9 @@ import idb from "@/assets/js/idb"
 
 function openIDB(){
 	if(!'serviceWorker' in navigator){
-	  return Promise.resolve();
+	  return Promise.resolve()
 	} else {
-		return idb.open('currencyDB', 1, upgradeDB => {
+		return idb.open('currencyDB', 3, upgradeDB => {
 			switch(upgradeDB.oldVersion){
 				case 0:
 					const currrencies = upgradeDB.createObjectStore('currencies')
@@ -22,13 +22,13 @@ function getCurrencies() {
 	  		if(data === undefined){
 			    return fetch('https://free.currencyconverterapi.com/api/v5/currencies')
 				        .then(response => {
-				          return response.json();
+				          return response.json()
 				        })
 				        .then(data => {
 				        	let currencies = data.results	
 							openIDB().then(db => {
-								let tx = db.transaction('currencies', 'readwrite');
-							    let store = tx.objectStore('currencies');
+								let tx = db.transaction('currencies', 'readwrite')
+							    let store = tx.objectStore('currencies')
 							    store.put(currencies, 'currencies')
 							})
 				    		return currencies
@@ -40,21 +40,31 @@ function getCurrencies() {
 }
 
 function getRate(fromCurrency, toCurrency){
-	// return openIDB().then(db => {
-	// 	const ratesStore = db.transaction(['rates']).objectStore('rates')
-	// 	return ratesStore.get(`${fromCurrency}_${toCurrency}`).then(data => {
-	// 		if (data === undefined){
-		return fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${fromCurrency}_${toCurrency}`)
-				.then(response => {
-					return response.json()
-				})
-				.then(data => {
-					console.log('rate Object: ', data.results.fromCurrency_toCurrency.val)
-					return 
-				})
-	// 		}
-	// 	})
-	// })
+	return openIDB().then(db => {
+		const ratesStore = db.transaction(['rates']).objectStore('rates')
+		return ratesStore.get(`${fromCurrency}_${toCurrency}`).then(data => {
+			if (data === undefined){
+				return fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${fromCurrency}_${toCurrency},${toCurrency}_${fromCurrency}`)
+						.then(response => {
+							return response.json()
+						})
+						.then(data => {
+							let forwardIDs = `${fromCurrency}_${toCurrency}`
+							let backwardIDs = `${toCurrency}_${fromCurrency}`
+							let forwardRate = data.results[forwardIDs].val
+							let backwardRate = data.results[backwardIDs].val
+							openIDB().then(db => {
+								let tx = db.transaction('rates', 'readwrite')
+								let store = tx.objectStore('rates')
+								store.put(forwardRate, forwardIDs)
+								store.put(backwardRate, backwardIDs)
+							})
+							return forwardRate
+						})
+			}
+			return data
+		})
+	})
 }
 
 export {getCurrencies, getRate}
