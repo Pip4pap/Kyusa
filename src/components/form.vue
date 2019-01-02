50326cf44c1f25cd87ba6a4212d32fc322ceb7d3
Musumba Phillip Mulindwa <phillip.musumba.52@gmail.com>
2019-01-02 15:35:17 +0300

    feat: Alert user when offline

--- src/components/form.vue
+++ src/components/form.vue
@@ -15,6 +15,15 @@
 	    </select><br/>
 		<button v-bind:class="{disabled: !(toCurrency && fromCurrency)}" @click="convert">Convert</button>
 		<p>{{toCurrency}} {{result}}</p>
+		<div id="popup" class="overlay">
+			<div class="popup">
+				<h2>You're offline</h2>
+				<span class="close" @click="closePopup">&times;</span>
+				<div class="content">
+					<strong>Only</strong> the conversions you made while online are available
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -41,9 +50,20 @@ export default {
   },
   methods: {
   	convert() {
+  		if(navigator.onLine === false){
+  			$('button').click(() => {
+  				$('.overlay').css({'visibility':'visible', 'opacity':'1'})
+  			})
+  		}
+
   		getRate(this.fromCurrency, this.toCurrency).then(rate => {
   			let result = +(rate * this.inputAmount).toFixed(0)
   			this.result = result.toLocaleString()
+  		})
+  	},
+  	closePopup(){
+  		$('.close').click(() => {
+  			$('.overlay').css({'visibility':'hidden', 'opacity':'0'})
   		})
   	}
   }
@@ -79,5 +99,58 @@ export default {
 	p{
 		font-size: 40px;
 		margin-top: 30px;
+	}
+	.overlay {
+	  position: fixed;
+	  top: 0;
+	  bottom: 0;
+	  left: 0;
+	  right: 0;
+	  background: rgba(0, 0, 0, 0.7);
+	  transition: opacity 500ms;
+	  visibility: hidden;
+	  opacity: 0;
+	}
+	.popup {
+	  margin: 70px auto;
+	  padding: 20px;
+	  background: #fff;
+	  border-radius: 5px;
+	  width: 30%;
+	  position: relative;
+	  transition: all 5s ease-in-out;
+	}
+
+	.popup h2 {
+	  margin-top: 0;
+	  color: #333;
+	  font-family: Tahoma, Arial, sans-serif;
+	}
+	.popup .close {
+	  position: absolute;
+	  top: 20px;
+	  right: 30px;
+	  transition: all 200ms;
+	  font-size: 30px;
+	  font-weight: bold;
+	  text-decoration: none;
+	  color: #333;
+	}
+	.popup .close:hover {
+	  color: #06D85F;
+	  cursor: pointer;
+	}
+	.popup .content {
+	  max-height: 30%;
+	  overflow: auto;
+	}
+
+	@media screen and (max-width: 700px){
+	  .box{
+	    width: 70%;
+	  }
+	  .popup{
+	    width: 70%;
+	  }
 	}
 </style>
